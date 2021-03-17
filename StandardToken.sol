2b42f3310f81a627b11c9965feeffcb303742f4b
Iwis Z <iwiszhou@gmail.com>
2021-03-16 21:46:00 -0400

    add comment

--- StandardToken.sol
+++ StandardToken.sol
@@ -10,6 +10,8 @@ interface IToken{
     function balanceOf(address _owner) external view returns(uint256);
 }
 
+/// @title A StandarToken
+/// @author Iwis Zhou
 contract StandardToken is Ownable,IToken{
     using SafeMath for uint256;
     
@@ -26,24 +28,31 @@ contract StandardToken is Ownable,IToken{
         balances[owner()] = 10; //init 10$ to the owner
     }
     
+    /// @notice Calculate tree age in years, rounded up, for live trees
+    /// @dev The Alexandr N. Tetearing algorithm could increase precision
+    /// @param rings The number of rings from dendrochronological sample
+    /// @return age in years, rounded up for partial years
     function mint(address _owner, uint256 _amount) public onlyOwner{
         totalSupplyVariable = totalSupplyVariable.add(_amount);
         balances[_owner] = _amount;
     }
     
+    /// @param address: receiver's address, uint256 - amount to transfer to receiver
+    /// @return true if transfer successfully
     function transfer(address _receipient, uint256 _amount) external override returns(bool){
         balances[msg.sender] = balances[msg.sender].sub(_amount);
         balances[_receipient] = balances[_receipient].add(_amount);
+        return true;
     }
     
+    /// @return current totalSupply number
     function totalSupply() external view override returns(uint256)  {
         return totalSupplyVariable;
     }
     
+    /// @param owner address
+    /// @return this address's balance
     function balanceOf(address _owner) external view override returns(uint256)  {
         return balances[_owner];
     }
-    
-    //contract owner - 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
-    // test A - 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
 }
