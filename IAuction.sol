b94a799f673ef44c773e173be86f69ec2c3be0ac
Iwis Z <iwiszhou@gmail.com>
2021-03-18 17:02:26 -0400

    clean up

--- IAuction.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-interface IAuction{
-    function placeBid() external payable returns(bool);
-    function withdraw() external payable returns(bool);
-    function cancelAuction() external returns(bool);
-}
